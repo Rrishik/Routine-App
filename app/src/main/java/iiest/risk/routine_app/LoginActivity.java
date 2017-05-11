eb842978f6a734ee8f5b1eac4e30768ac50cb7fb
Rrishik <ramenarishik@gmail.com>
2017-05-11 13:47:47 +0530

    Silent Sign In added

--- app/src/main/java/iiest/risk/routine_app/LoginActivity.java
+++ app/src/main/java/iiest/risk/routine_app/LoginActivity.java
@@ -1,11 +1,13 @@
 package iiest.risk.routine_app;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -14,6 +16,8 @@ import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.api.ResultCallback;
 
 
 public class LoginActivity extends AppCompatActivity implements
@@ -23,6 +27,8 @@ public class LoginActivity extends AppCompatActivity implements
     private static final String TAG = "LoginActivity";
     private GoogleApiClient mGoogleApiClient;
     private SignInButton mSignInButton;
+    private ProgressDialog mProgressDialog;
+    private TextView mUserView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,11 +52,41 @@ public class LoginActivity extends AppCompatActivity implements
                 signIn();
             }
         });
+        mUserView = (TextView) findViewById(R.id.textViewUser);
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(mGoogleApiClient);
+        if (opr.isDone()) {
+            // If the user's cached credentials are valid, the OptionalPendingResult will be "done"
+            // and the GoogleSignInResult will be available instantly.
+            Log.d(TAG, "Got cached sign-in");
+            GoogleSignInResult result = opr.get();
+            handleSignInResult(result);
+        } else {
+            // If the user has not previously signed in on this device or the sign-in has expired,
+            // this asynchronous branch will attempt to sign in the user silently.  Cross-device
+            // single sign-on will occur in this branch.
+            showProgressDialog(true);
+            opr.setResultCallback(new ResultCallback<GoogleSignInResult>() {
+                @Override
+                public void onResult(GoogleSignInResult googleSignInResult) {
+                    showProgressDialog(false);
+                    handleSignInResult(googleSignInResult);
+                }
+            });
+        }
+    }
+
 
     private void signIn() {
         Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
         startActivityForResult(signInIntent, RC_SIGN_IN);
+        showProgressDialog(true);
+
     }
 
     @Override
@@ -66,19 +102,39 @@ public class LoginActivity extends AppCompatActivity implements
     private void handleSignInResult(GoogleSignInResult result) {
         if (result.isSuccess()) {
             GoogleSignInAccount account = result.getSignInAccount();
-            updateUI(true);
-        }
-
+            updateUI(true, account.getDisplayName());
+        } else
+            updateUI(false, null);
+        showProgressDialog(false);
     }
 
-    private void updateUI(boolean b) {
-        if (b) Log.d(TAG, "updateUI: Login Success");
-        else Log.d(TAG, "updateUI: Login Failiure");
+    private void updateUI(boolean b, String actNm) {
+        if (b) {
+            Log.d(TAG, "updateUI: Login Success " + actNm);
+            mUserView.setText(actNm);
+        } else {
+            Log.d(TAG, "updateUI: Login Failiure");
+            mUserView.setText("Not logged in");
+        }
+    }
+
+    private void showProgressDialog(boolean b) {
+        if (b) {
+            if (mProgressDialog == null) {
+                mProgressDialog = new ProgressDialog(this);
+                mProgressDialog.setMessage(getString(R.string.loading));
+                mProgressDialog.setIndeterminate(true);
+            }
+            mProgressDialog.show();
+        } else if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.hide();
+        }
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
         Log.d(TAG, "onConnectionFailed: " + connectionResult);
     }
+
 }
 
